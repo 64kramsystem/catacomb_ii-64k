52c4554c9244294316b7293e6ab161f758e55222
Saverio Miroddi <saverio.pub2@gmail.com>
2022-08-14 11:17:46 +0200

    Remove unused global `_maxplayers`

--- src/catacomb.rs
+++ src/catacomb.rs
@@ -34,7 +34,6 @@ extern "C" {
     fn _checkhighscore();
     fn _showhighscores();
     static mut _extension: *const libc::c_char;
-    static mut _maxplayers: libc::c_int;
     static mut score: sdword;
     fn printhighscore();
     fn printbody();
@@ -1498,8 +1497,6 @@ pub fn original_main() {
         //   puts ("CATACOMB II is executing");
 
         //  _dontplay = 1;	// no sounds for debugging and profiling
-
-        _maxplayers = 1;
 
         _extension = b"CA2\0" as *const u8 as *const libc::c_char;
 
