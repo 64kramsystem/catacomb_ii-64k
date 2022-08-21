56d797058a5f3e542813bbddeb1987e3a31546c7
Saverio Miroddi <saverio.pub2@gmail.com>
2022-08-21 22:46:09 +0200

    Add `p_m_serde` dependency, and implement `Deserialize` for `pic_type`

--- src/pic_type.rs
+++ src/pic_type.rs
@@ -6,3 +6,19 @@ pub struct pictype {
     pub shapeptr: u32,
     pub name: [i8; 8],
 }
+
+impl Deserialize for pictype {
+    fn deserialize<R: std::io::Read>(mut r: R) -> Self {
+        let width = Deserialize::deserialize(&mut r);
+        let height = Deserialize::deserialize(&mut r);
+        let shapeptr = Deserialize::deserialize(&mut r);
+        let name = Deserialize::deserialize(&mut r);
+
+        Self {
+            width,
+            height,
+            shapeptr,
+            name,
+        }
+    }
+}
