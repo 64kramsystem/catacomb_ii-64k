2d04ccc106d41bf791ee1b3964dc2449879a4f32
Saverio Miroddi <saverio.pub2@gmail.com>
2022-08-22 13:15:50 +0200

    Use `Deserialize` procedural macro for `Deserialize` trait implementation

--- src/pic_type.rs
+++ src/pic_type.rs
@@ -1,25 +1,9 @@
-use p_m_serde::Deserialize;
+use p_m_serde_macros_lib::Deserialize;
 
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Deserialize)]
 pub struct pictype {
     pub width: i16,
     pub height: i16,
     pub shapeptr: u32,
     pub name: [i8; 8],
-}
-
-impl Deserialize for pictype {
-    fn deserialize<R: std::io::Read>(mut r: R) -> Self {
-        let width = Deserialize::deserialize(&mut r);
-        let height = Deserialize::deserialize(&mut r);
-        let shapeptr = Deserialize::deserialize(&mut r);
-        let name = Deserialize::deserialize(&mut r);
-
-        Self {
-            width,
-            height,
-            shapeptr,
-            name,
-        }
-    }
 }
