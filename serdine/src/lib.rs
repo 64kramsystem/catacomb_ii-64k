3fa15882afba930aa6ea8a7f5e78a3fd1f856387
Saverio Miroddi <saverio.pub2@gmail.com>
2022-08-23 12:48:07 +0200

    Remove serdine code, and make it a dependency

--- serdine/src/lib.rs
+++
@@ -1,9 +0,0 @@
-// Poor Man's Serde. Trivial pair of interfaces for de/serialization.
-
-mod builtin_types;
-mod deserialize;
-mod macros;
-mod serialize;
-
-pub use deserialize::Deserialize;
-pub use serialize::Serialize;
