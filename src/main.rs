902991cb4c276a91211331caa2b3898a62d26c61
Saverio Miroddi <saverio.pub2@gmail.com>
2022-09-02 12:14:31 +0200

    Fix clippy::identity_op

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@
     non_upper_case_globals,
     unused_assignments,              // there are a lot, and can't be automatically fixed
     clippy::expect_fun_call,         // meh
-    clippy::identity_op,
+    clippy::identity_op,             // actually more readable
     clippy::int_plus_one,
     clippy::missing_safety_doc,
     clippy::nonminimal_bool,         // actually more readable
